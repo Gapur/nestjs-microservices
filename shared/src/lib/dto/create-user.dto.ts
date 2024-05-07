8ada6f45d77da9695098bdabffab27b900aa6724
Gapur Kassym <gapur.kassym@gmail.com>
2024-05-07 23:26:28 +0300

    delete email field

--- shared/src/lib/dto/create-user.dto.ts
+++ shared/src/lib/dto/create-user.dto.ts
@@ -1,12 +1,9 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateUserDto {
   @IsString()
   @IsNotEmpty()
   username: string;
-
-  @IsEmail()
-  email: string;
 
   @IsNotEmpty()
   password: string;
--- shared/src/lib/entities/user.entity.ts
+++ shared/src/lib/entities/user.entity.ts
@@ -1,6 +1,5 @@
 export class User {
   id?: number;
   username: string;
-  email: string;
   password: string;
 }
